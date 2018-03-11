973fcff44a3598aacc767465effb124e724f9bd8
Marcus Hunger <marcus.hunger@gmail.com>
2018-03-11 19:47:37 +0700

    try to fix dockerhub connection refused error by using 127.0.0.1 instead of localhost

--- import-notebook.sh
+++ import-notebook.sh
@@ -5,7 +5,7 @@ set -e
 /zeppelin/bin/zeppelin.sh &
 ZEPPID=$!
 sleep 20
-curl -d@/mnistdemo-notebook.json http://localhost:8080/api/notebook/import
+curl -d@/mnistdemo-notebook.json http://127.0.0.1:8080/api/notebook/import
 sleep 1
 kill $ZEPPID
 sleep 2
